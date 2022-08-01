d87360dadd04e1ea5acfa898a9baf8ca1efff3d2
Abdul Majid <majidp@Abduls-MacBook-Air.local>
2022-08-01 13:42:35 +0530

    header updated

--- src/Header.js
+++ src/Header.js
@@ -5,7 +5,7 @@ function Header(){
     <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
       <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
 
-        <span className="fs-4">Simple header</span>
+        <span className="fs-4">Hello World</span>
       </a>
 
       <ul className="nav nav-pills">
